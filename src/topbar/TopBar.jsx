1a15334ea8fbdfede6418685f06c6365e9cd92f9
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-05-10 12:47:09 +0530

    topbarupdate

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,9 +3,25 @@ import './topbar.css'
 export default function TopBar() {
   return (
     <div className='top'>
-      <div className="topleft">l</div>
-      <div className="topcentre">c </div>
-      <div className="topright">r </div>   
+      <div className="topleft">
+      <i className="topicon fa-brands fa-square-facebook"></i>
+      <i className="topicon fa-brands fa-square-twitter"></i>
+      <i className="topicon fa-brands fa-square-instagram"></i>
+      <i className="topicon fa-brands fa-square-linkedin"></i>
+      </div>
+      <div className="topcentre">
+        <ul className="topList">
+        <li className="topListItem">HOME</li>
+        <li className="topListItem">ABOUT</li>
+        <li className="topListItem">CONTACT</li>
+        <li className="topListItem">WRITE</li>
+        <li className="topListItem">LOGOUT</li>
+        </ul>
+         </div>
+      <div className="topright">
+        <img className="topImg" src="https://media.licdn.com/dms/image/C5603AQH3HiDN-wwNjg/profile-displayphoto-shrink_800_800/0/1615975966329?e=2147483647&v=beta&t=8S_JJLBR6kBpLrKwHSP6BJyVTvtZl5ZS_Wf1uDvlwQU" alt="" />
+        <i className="topSearchIcon fa-solid fa-search"></i>
+         </div>   
       
 
       </div>
