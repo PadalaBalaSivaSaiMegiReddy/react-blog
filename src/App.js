8770abdc8db5c017370d96db3abd26acf1d78e70
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-05-28 15:11:16 +0530

    update settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
 import TopBar from "./components/topbar/TopBar";
-import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+// import Write from "./pages/write/Write";
 
 
 function App() {
   return (<>
   <TopBar />
-  <Write />
+  <Settings />
 
   </>
     
