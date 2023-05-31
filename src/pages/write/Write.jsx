e43fe0b6899e658fc2c44b2399f2f56f8ee923ca
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-05-31 23:39:17 +0530

    fix

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -3,7 +3,8 @@ import "./write.css";
 export default function Write() {
 	return (
 		<div className="write">
-			<img className="writeimg"
+			<img
+				className="writeimg"
 				src="https://images.unsplash.com/photo-1684785459021-761c8ee14e49?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
 				alt=""
 			/>
