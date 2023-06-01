979f7261d13b287982efad9362e3e64a82e4d675
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-06-01 12:59:22 +0530

    changes

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+//dotenv
+require("dotenv/config");
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
