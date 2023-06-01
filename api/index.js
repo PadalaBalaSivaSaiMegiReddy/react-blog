44da235f7f9d7a8fe7187c2c48ac702a7a2e0079
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-06-01 17:04:47 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 //dotenv
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL,{
 
 app.use("api/auth", authRoute);
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+app.listen(5000, () => {
+	console.log("Server is running on port 5000");
 });
+
--- api/models/User.js
+++ api/models/User.js
@@ -3,20 +3,16 @@ const UserSchema = new mongoose.Schema({
     username:{
         type: String,
         required: true,
-        min: 3,
-        max: 20,
         unique: true
     },
     email:{
         type: String,
         required: true,
-        max: 50,
         unique: true
     },
     password:{
         type: String,
         required: true,
-        min: 6
     },
     profilePicture:{
         type: String,
