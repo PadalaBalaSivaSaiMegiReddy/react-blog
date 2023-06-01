3a758698c11d83a700dd86c9470b9e8dafa360ed
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-06-01 15:00:19 +0530

    files

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 //dotenv
 require("dotenv/config");
+app.use(express.json());
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
+
+
+app.use("api/auth", authRoute);
 
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
