9296c1fb12fb575563ca2e81b75210fe716920aa
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-01-31 20:45:09 +0530

    Add about me section to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,18 @@ import './sidebar.css'
 
 const SideBar = () => {
   return (
-    <div className='sidebar'>SideBar</div>
+    <div className='sidebar'>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="" height={500} width={500} />
+        <p>
+          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+          amet ex esse.Sunt eu ut nostrud id quis proident.
+        </p>
+      </div>
+    </div>
   )
 }
 
