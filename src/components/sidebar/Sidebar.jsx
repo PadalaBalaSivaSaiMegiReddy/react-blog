b3026876c2a1ddb38123e94873e834d727bc28fc
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-01 20:28:09 +0530

    Update image dimensions in Sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,7 +7,7 @@ const SideBar = () => {
         <span className="sidebarTitle">ABOUT ME</span>
         <img
           src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="" height={500} width={500} />
+          alt="" height={550} width={550} />
         <p>
           Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
           amet ex esse.Sunt eu ut nostrud id quis proident.
