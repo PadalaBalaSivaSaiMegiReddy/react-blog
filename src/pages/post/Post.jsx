6214ce732cdb52e0dba3bc6d4a4159102836da50
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-05 22:46:49 +0530

    Delete TopBar component and its associated CSS file, and add multiple instances of Post component to Posts component

---
+++ src/pages/post/Post.jsx
@@ -0,0 +1,18 @@
+import "./post.css"
+export default function Post() {
+  return (
+    <div className="post">
+        <img src="https://images.unsplash.com/photo-1611307742746-43cbea512c37?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww" alt="" className="postImg"/>
+
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">Lorem ipsum dolor sit amet</span>
+            <hr/>
+            <span className="postDate">1 hour ago</span>
+    </div>
+    </div>
+  )
+}
