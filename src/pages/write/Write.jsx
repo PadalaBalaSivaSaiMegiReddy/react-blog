c1b82e34485789ae7a0d0000f63c909a49c39747
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-07 22:37:58 +0530

    Add image and adjust styles in Write component

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,7 +1,9 @@
 import "./write.css"
 export default function Write() {
   return (
-    <div className="write"><form className="writeForm">
+    <div className="write">
+      <img className="writeImg" src="https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg" alt="" />
+      <form className="writeForm">
       <div className="writeFormGroup">
         <label htmlFor="fileInput">
           <i className="writeIcon fas fa-plus"></i>
