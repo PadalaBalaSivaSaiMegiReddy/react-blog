dfcabdef74f061aa146bf6ba0a5c37b3b383509b
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-08 23:22:53 +0530

    Add Settings page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Topbar from "./components/topbar/Topbar"
 import Home from "./pages/home/Home"
+import Settings from "./pages/settings/Settings"
 import Single from "./pages/single/Single"
 import Write from "./pages/write/Write"
 
@@ -8,7 +9,7 @@ const App = () => {
   return (
     <>
     <Topbar/>
-    <Write/>   
+    <Settings/>  
      </>
   )
 }
