5c87303e61a1ef15ddfa7d9d44ffb0991237bacd
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-09 22:37:34 +0530

    Add settings title and update profile picture styling

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -7,10 +7,11 @@ export default function Settings() {
         <div className="settingsTitle">
             <span className="settingsUpdateTitle">Update Your Account</span>
             <span className="settingsDeleteTitle">Delete Your Account</span>
+            </div>
             <form  className="settingsForm">
                 <label >Profile Picture</label>
                 <div className="settingsPP">
-                <img src="https://images.unsplash.com/photo-1617194369222-af8a7e682365?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
+                <img className="settingsImg" src="https://images.unsplash.com/photo-1617194369222-af8a7e682365?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
                 <label htmlFor="fileInput">
                 <i className=" settingsPPIcon far fa-user-circle"></i>
                 </label>
@@ -24,7 +25,6 @@ export default function Settings() {
                 <input type="password"  />
                 <button className="settingsSubmit">Update</button>
             </form>
-        </div>
         </div>
         <Sidebar/>
         </div>
