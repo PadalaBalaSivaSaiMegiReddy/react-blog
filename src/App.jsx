74842b1c59476db7ec961bb756015649f71a1d6f
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-11 15:27:16 +0530

    Update user access to settings route

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     <Route path="/register" element={user?<Home/>:<Register/>}/>  
     <Route path="/login" element={user?<Home/>:<Login/>}/>  
     <Route path="/write" element={user?<Write/>:<Register/>}/> 
-    <Route path="/settings" element={<Settings/>}/>  
+    <Route path="/settings" element={user?<Settings/>:<Register/>}/>  
     <Route path="/post/:postId" element={<Single/>}/>  
 
  
