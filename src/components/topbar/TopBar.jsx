5f70f92ebf9cbf0c19ce6a04c1788ed4fba3d504
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-11 15:45:57 +0530

    Add links to topbar and update styles

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
 import './topbar.css';
 const TopBar = () => {
+  const user=false;
   return (
     <div className='top'>
       <div className="topLeft">
@@ -10,16 +12,25 @@ const TopBar = () => {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem"><Link to="/" className='link'>HOME</Link></li>
+          <li className="topListItem"><Link to="/" className='link'>ABOUT</Link></li>
+          <li className="topListItem"><Link to="/" className='link'>CONTACT</Link></li>
+          <li className="topListItem"><Link to="/write" className='link'>WRITE</Link></li>
+          <li className="topListItem">{user&&"LOGOUT"}</li>
         </ul>
       </div>
       <div className="topRight">
-      <img src="src/assets/LinkedIn_DP.jpg " alt="" className='topImg' />
-      <i className=" topSearchIcon fa-solid fa-magnifying-glass"></i>
+        {user?(<img src="src/assets/LinkedIn_DP.jpg " alt="" className='topImg' />
+      ):
+      <>
+      <Link to="/login" className='link'>LOGIN</Link>
+      <Link to="/register" className='link'>REGISTER</Link>
+      </>
+    
+      }
+
+<i className=" topSearchIcon fa-solid fa-magnifying-glass"></i>
+      
       </div>
     </div>
   )
