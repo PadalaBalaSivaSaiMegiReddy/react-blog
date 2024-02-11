657b2394e409ff8c3b65c4ffbed096cc149b1db0
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-11 12:51:32 +0530

    Add Register page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Topbar from "./components/topbar/Topbar"
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 import Settings from "./pages/settings/Settings"
 import Single from "./pages/single/Single"
 import Write from "./pages/write/Write"
@@ -10,7 +11,8 @@ const App = () => {
   return (
     <>
     <Topbar/>
-<Login/>     </>
+    <Register/>
+       </>
   )
 }
 
