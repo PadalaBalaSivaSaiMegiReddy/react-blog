55b03720fed6d34fd8cab2bf55ae5a974f11226f
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-12 04:23:39 +0530

    Add MongoDB connection and environment configuration

--- api/index.js
+++ api/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app=express();
+const dotevn=require('dotenv');
+const mongoose=require('mongoose');
+
+
+dotevn.config()
+
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => {
+		console.log("App connected to database");
+		app.listen(PORT, () => {
+			console.log(`App is listening to port: ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
 
 
 
