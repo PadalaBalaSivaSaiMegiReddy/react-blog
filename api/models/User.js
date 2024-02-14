32df2a9dbc03a89a09245237cdc946de0d761b0d
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-14 14:22:14 +0530

    Add User model to API

---
+++ api/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique:true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique:true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilepicture: {
+        type:String,
+        default:"",
+
+    },
+},
+{timestamp:true}
+);
+
+export const Book = mongoose.model('User', UserSchema);
