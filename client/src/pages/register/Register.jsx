bc0961f66692904736886239db12bb4f989bec2c
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-28 14:22:02 +0530

    Fix registration form and handle error

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,16 +1,26 @@
 import { Link } from 'react-router-dom'
 import './register.css'
 import { useState } from 'react'
+import axios from 'axios';
 
 export default function Register() {
   const [username,setUsername]=useState('');
   const [email,setEmail]=useState('');
-  const [password,setPasswrod]=useState('');
-  console.log(username);
-  console.log(email);
-  console.log(password);
-  function handleSubmit(e){
-    e.preventDefault()
+  const [password,setPassword]=useState('');
+  const [error,setError]=useState(false);
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    setError(false)
+    try{
+      const res=await axios.post('http://localhost:5000/api/auth/register',{
+        username,
+        email,
+        password,
+      });
+      res.data&&window.location.replace("/login")
+    }catch(e){
+      setError(true)
+    }
 
   }
 
@@ -21,14 +31,15 @@ export default function Register() {
         <label>Username</label>
             <input onChange={e=>setUsername(e.target.value)} type="text" placeholder='Enter Your Username...' />
             <label>Email</label>
-            <input onChange={e=>setUsername(e.target.value)} type="email" placeholder='Enter Your Email...' />
+            <input onChange={e=>setEmail(e.target.value)} type="email" placeholder='Enter Your Email...' />
             <label>Password</label>
-            <input  onChange={e=>setUsername(e.target.value)}  type="password" placeholder='Enter Your Password...' />
+            <input  onChange={e=>setPassword(e.target.value)}  type="password" placeholder='Enter Your Password...' />
             <button className="registerButton">Register</button>
         </form>
             <button type='submit' className="registerLoginButton">
               <Link to="/login" className='link'>Login</Link>
             </button>
+            {error&&<span style={{color: 'red',marginTop:"10px"}}>Something went wrong!</span>}
     </div>
   )
 }
