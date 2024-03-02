5472fcc053de44dd57d422cb110896393aadebb2
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-02 21:42:21 +0530

    Add path module to package.json and update image URLs

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -6,6 +6,8 @@ export default function SinglePost() {
     const location = useLocation();
     const path=location.pathname.split('/')[2];
     const [post,setPost]=useState({})
+    const PF='http://localhost:5000/images/'
+
     useEffect(()=>{
         const getPost= async()=>{
             const res = await axios.get("http://localhost:5000/api/posts/"+path);
@@ -15,12 +17,12 @@ export default function SinglePost() {
         getPost()
         
     },[path])
-    console.log(post)
+    console.log(PF+post.photo)
   return (
     <div className='singlePost'>
         <div className='singlePostWrapper'>
             {post.photo &&
-            (<img className='singlePostImg' src={post.photo} alt='' />)
+            (<img className='singlePostImg' src={PF+post.photo} alt='' />)
             }
             <h1 className='singlePostTitle'>
                 {post.title}
